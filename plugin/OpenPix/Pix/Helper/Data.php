6d9d1688965a61aec31c65f760cce44c7d3d37dd
daniloab <daniloassis.ti@gmail.com>
2021-06-10 13:47:49 -0300

    feat(pix): fix uuid function, grand total

--- plugin/OpenPix/Pix/Helper/Data.php
+++ plugin/OpenPix/Pix/Helper/Data.php
@@ -23,7 +23,9 @@ class Data extends AbstractHelper
      * @var ScopeConfigInterface
      */
 
-    const OPENPIX_ENV = 'development';
+    //    const OPENPIX_ENV = 'development';
+    //    const OPENPIX_ENV = 'staging';
+        const OPENPIX_ENV = 'production';
 
     // change this to work in development, staging or production
     /**
@@ -32,8 +34,6 @@ class Data extends AbstractHelper
      * @var Logger
      */
     protected $_openpixLogger;
-    //    const OPENPIX_ENV = 'staging';
-    //    const OPENPIX_ENV = 'production';
 
     /**
      * Data constructor.
@@ -82,32 +82,26 @@ class Data extends AbstractHelper
             '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
 
             // 32 bits for "time_low"
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
+            random_int(0, 0xffff),
+            random_int(0, 0xffff),
 
             // 16 bits for "time_mid"
-            mt_rand(0, 0xffff),
+            random_int(0, 0xffff),
 
             // 16 bits for "time_hi_and_version",
             // four most significant bits holds version number 4
-            mt_rand(0, 0x0fff) | 0x4000,
+            random_int(0, 0x0fff) | 0x4000,
 
             // 16 bits, 8 bits for "clk_seq_hi_res",
             // 8 bits for "clk_seq_low",
             // two most significant bits holds zero and one for variant DCE1.1
-            mt_rand(0, 0x3fff) | 0x8000,
+            random_int(0, 0x3fff) | 0x8000,
 
             // 48 bits for "node"
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff),
-            mt_rand(0, 0xffff)
+            random_int(0, 0xffff),
+            random_int(0, 0xffff),
+            random_int(0, 0xffff)
         );
-    }
-
-    public function console_log($message)
-    {
-        print_r($message);
-        echo '<br />';
     }
 
     /**
