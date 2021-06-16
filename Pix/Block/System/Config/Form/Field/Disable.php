b20f68843f55e4f9b557708be2b6204266a9eae6
daniloab <daniloassis.ti@gmail.com>
2021-06-15 21:56:40 -0300

    feat(openpix): following vindi

---
+++ Pix/Block/System/Config/Form/Field/Disable.php
@@ -0,0 +1,13 @@
+<?php
+namespace OpenPix\Pix\Block\System\Config\Form\Field;
+
+use Magento\Framework\Data\Form\Element\AbstractElement;
+
+class Disable extends \Magento\Config\Block\System\Config\Form\Field
+{
+    protected function _getElementHtml(AbstractElement $element)
+    {
+        $element->setDisabled('disabled');
+        return $element->getElementHtml();
+    }
+}
