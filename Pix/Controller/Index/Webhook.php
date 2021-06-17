5cd596daf0e05f026f6d02153c78fb214cf0b98f
daniloab <daniloassis.ti@gmail.com>
2021-06-16 21:20:11 -0300

    feat(webhook): handling error and return

--- Pix/Controller/Index/Webhook.php
+++ Pix/Controller/Index/Webhook.php
@@ -4,23 +4,27 @@ namespace OpenPix\Pix\Controller\Index;
 
 use OpenPix\Pix\Helper\Data;
 use OpenPix\Pix\Helper\WebhookHandler;
+use Magento\Framework\Controller\Result\JsonFactory;
 
 class Webhook extends \Magento\Framework\App\Action\Action
 {
     protected $_pageFactory;
     private $webhookHandler;
+    private $resultJsonFactory;
 
     public function __construct(
         \Psr\Log\LoggerInterface $logger,
         WebhookHandler $webhookHandler,
         Data $helperData,
         \Magento\Framework\App\Action\Context $context,
-        \Magento\Framework\View\Result\PageFactory $pageFactory
+        \Magento\Framework\View\Result\PageFactory $pageFactory,
+        JsonFactory $resultJsonFactory
     ) {
         $this->logger = $logger;
         $this->_pageFactory = $pageFactory;
         $this->webhookHandler = $webhookHandler;
         $this->helperData = $helperData;
+        $this->resultJsonFactory = $resultJsonFactory;
         return parent::__construct($context);
     }
 
@@ -29,6 +33,7 @@ class Webhook extends \Magento\Framework\App\Action\Action
      */
     public function execute()
     {
+        $resultJson = $this->resultJsonFactory->create();
         $this->logger->debug(__(sprintf('Start webhook')));
 
         if (!$this->validateRequest()) {
@@ -36,18 +41,25 @@ class Webhook extends \Magento\Framework\App\Action\Action
 
             $this->logger->error(__(sprintf('Invalid webhook attempt from IP %s', $ip)));
 
-            header('HTTP/1.2 400 Bad Request');
-            $response = [
-                'error' => 'Invalid Webhook Authorization',
-            ];
+            $resultJson->setHttpResponseCode(400);
 
-            return json_encode($response);
+            return $resultJson->setData([
+                'error' => 'Invalid Webhook Authorization',
+            ]);
         }
 
         $body = file_get_contents('php://input');
         $this->logger->info(__(sprintf("Webhook New Event!\n%s", $body)));
 
-        $this->webhookHandler->handle($body);
+       $result = $this->webhookHandler->handle($body);
+
+       if(isset($result["error"])) {
+           $resultJson->setHttpResponseCode(400);
+           return $resultJson->setData([ "error" => $result["error"]]);
+       }
+
+        $resultJson->setHttpResponseCode(200);
+        return $resultJson->setData([ "success" => $result["success"]]);
     }
 
     /**
