562647d1fd8e131342c588635beb6e24c17e9b51
Danilo <daniloassis.ti@gmail.com>
2022-02-09 17:45:55 -0300

    fix(jest): jest

--- jest.config.js
+++ jest.config.js
@@ -1,10 +1,17 @@
 const { defaults } = require('jest-config');
+const pkg = require('./package');
 
 module.exports = {
+  name: pkg.name,
+  displayName: pkg.name,
+  setupFilesAfterEnv: ['<rootDir>/test/setupTestFramework.js'],
   testPathIgnorePatterns: ['/node_modules/', './dist'],
   transformIgnorePatterns: ['node_modules/(?!d3-random)'],
   moduleFileExtensions: [...defaults.moduleFileExtensions, 'ts', 'tsx'],
+  resetModules: false,
   transform: {
-    '^.+\\.(js|ts|tsx)?$': '<rootDir>/test/babelJestUpward',
+    '^.+\\.(js|ts|tsx)?$': 'babel-jest',
   },
+  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.(js|ts|tsx)?$',
+  cacheDirectory: '.jest-cache',
 };
