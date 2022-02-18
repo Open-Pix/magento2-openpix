48d33117bd03b0371b36768f822e731e56720ac3
Danilo <daniloassis.ti@gmail.com>
2022-02-18 10:55:02 -0300

    feat(vite): more vite plugin js

--- checkout-success/main.js
+++ checkout-success/main.js
@@ -1,23 +1,15 @@
 (function () {
   document.addEventListener('DOMContentLoaded', function () {
-    const btnCopyEmv = document.querySelector('#btnCopyEmv');
+    const script = document.createElement('script');
 
-    if (btnCopyEmv) {
-      btnCopyEmv.addEventListener('click', function () {
-        const textAreaEmv = document.querySelector('#emv');
+    script.id = 'plugin';
+    script.src =
+      'http://localhost:4444/openpix.js?appID=Q2xpZW50X0lkX2RiMjVlYmY0LTZkOTQtNGY2ZS1hYzdkLTFhOGI3Y2ZlOGY2YTpDbGllbnRfU2VjcmV0X3IvbU16R3p1bmpvSEVseVFFeUw1LzVtYU9TQW9PaXpFaTh1NFgzeHBxU2c9&correlationID=f4060b5e-0f20-4fdf-91b2-b99e01fe239e&node=openpix-order';
+    script.async = true;
+    script.onload = function () {
+      console.log('script loaded, you can use it now.');
+    };
 
-        textAreaEmv.select();
-        textAreaEmv.setSelectionRange(0, 99999);
-        document.execCommand('copy');
-        document.getSelection().collapseToEnd();
-
-        const originalText = btnCopyEmv.innerHTML;
-        btnCopyEmv.innerHTML = 'Copiado!';
-
-        setTimeout(function () {
-          btnCopyEmv.innerHTML = originalText;
-        }, 10 * 1000);
-      });
-    }
+    document.body.append(script);
   });
 })();
