8aa3c0e9896d4195aa090bd989866444069c5ed6
Eduardo Maciel <54045014+edumaciel10@users.noreply.github.com>
2022-04-27 09:48:01 -0300

    chore(clean): remove unused imports (#225)

--- config/OpenPixConfigProduction.php
+++ config/OpenPixConfigProduction.php
@@ -1,22 +1,6 @@
 <?php
 
 namespace OpenPix\Pix\Helper;
-
-use Magento\Checkout\Model\Session;
-use Magento\Customer\Model\Customer;
-use Magento\Framework\App\Config\ScopeConfigInterface;
-use Magento\Framework\App\Helper\AbstractHelper;
-use Magento\Framework\App\Helper\Context;
-use Magento\Framework\App\ProductMetadataInterface;
-use Magento\Framework\HTTP\Client\Curl;
-use Magento\Framework\HTTP\PhpEnvironment\RemoteAddress;
-use Magento\Framework\Module\ModuleListInterface;
-use Magento\Framework\Serialize\SerializerInterface;
-use Magento\Store\Model\ScopeInterface;
-use Magento\Store\Model\StoreManagerInterface;
-use Magento\Framework\App\Config\Storage\WriterInterface;
-use Magento\Framework\App\Cache\TypeListInterface;
-
 class OpenPixConfig
 {
     const OPENPIX_ENV = 'production';
