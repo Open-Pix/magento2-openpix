2384bffc09c8eb5a025a905d1d89484cfdb20874
criskell <96352451+criskell@users.noreply.github.com>
2023-07-26 10:30:34 -0300

    feat(checkout): add parcelado payment option (#647)

---
+++ Pix/view/frontend/web/js/view/payment/openpix_pix_parcelado.js
@@ -0,0 +1,15 @@
+define([
+    'uiComponent',
+    'Magento_Checkout/js/model/payment/renderer-list',
+], function (Component, rendererList) {
+    'use strict';
+    rendererList.push(
+        {
+            type: 'openpix_pix_parcelado',
+            component: 'OpenPix_Pix/js/view/payment/method-renderer/openpix-pix-parcelado-method',
+        },
+        // other payment method renderers if required
+    );
+    /** Add view logic here if needed */
+    return Component.extend({});
+});
